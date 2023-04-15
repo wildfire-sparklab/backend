b33bd79c1b3106bf3e2054982b018a101575be09
1Suro1 <timu.zaxarov@gmail.com>
2023-04-15 17:03:25 +0900

    edit

--- cmd/main.go
+++ cmd/main.go
@@ -57,9 +57,27 @@ func main() {
 	checkerService.StartCheck()
 
 	r := gin.Default()
+	r.Use(CORSMiddleware())
+	gin.SetMode(gin.ReleaseMode)
 	hotspotHandler := hotspots.NewHotSpotHandler(hotspotStorage)
 	s3handler := s3.NewS3Handler(s3Client)
 	s3handler.Register(r)
 	hotspotHandler.Register(r)
 	r.Run(":8081")
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
