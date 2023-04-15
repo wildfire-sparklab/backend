30393863f7af19567b3e1c92e0cb9ac3d0328866
1Suro1 <timu.zaxarov@gmail.com>
2023-04-15 21:51:22 +0900

    edit

--- internal/s3/handler.go
+++ internal/s3/handler.go
@@ -23,6 +23,30 @@ func NewS3Handler(client s32.Client) handlers.Handler {
 
 func (h handler) Register(router *gin.Engine) {
 	router.GET("tiles/:date/:z/:x/:y", h.GetTiles)
+	router.GET("perimeters/:date/:file", h.GetPerimeters)
+}
+
+func (h *handler) GetPerimeters(ctx *gin.Context) {
+	input := &s3.GetObjectInput{
+		Bucket: aws.String(h.client.Bucket),
+		Key:    aws.String(ctx.Param("date") + "/" + ctx.Param("file")),
+	}
+	object, err := h.client.Client.GetObject(input)
+	if err != nil {
+		fmt.Println(err)
+		ctx.String(404, "not found")
+		return
+	}
+	if object.Body != nil {
+		defer object.Body.Close()
+	}
+	if object.ContentLength == nil {
+		return
+	}
+	ctx.Header("Content-Type", *object.ContentType)
+	ctx.Header("Content-Length", fmt.Sprintf("%d", *object.ContentLength))
+	ctx.Status(http.StatusOK)
+	io.Copy(ctx.Writer, object.Body)
 }
 
 func (h *handler) GetTiles(ctx *gin.Context) {
